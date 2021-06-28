851f729bd1a7acaa99ca3298039e6fc9777c12ef
stark136 <81159077+stark136@users.noreply.github.com>
2021-06-28 06:22:49 +0530

    Update urls.py

--- cchs/urls.py
+++ cchs/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+
+admin.site.site_header = "CCHS Admin Page"
+admin.site.site_title = "Christ chruch Admin Portal"
+admin.site.index_title = "Welcome to cchs admin pannel" 
+
 urlpatterns = [
     path('admin/', admin.site.urls)
     admin.site.site_header = "CCHS Admin Page"
